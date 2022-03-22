227a6a612ba0ccd232088d196ed6ae752e29491d
CCARRILLO <cristiancamilo.carrillo@ynov.com>
2022-03-22 14:13:09 +0100

    container-buttons

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"fmt"
 	"html/template"
 	"io/ioutil"
 	"log"
@@ -64,10 +65,10 @@ type Data struct {
 }
 
 func main() {
-	//http://ddragon.leagueoflegends.com/cdn/9.3.1/data/img/champion.png
 	url := "http://ddragon.leagueoflegends.com/cdn/12.5.1/data/fr_FR/champion.json"
-	//http://ddragon.leagueoflegends.com/cdn/12.5.1/data/champion.json
-	//https://ddragon.leagueoflegends.com/realms/na.json
+
+	imageServer := http.FileServer(http.Dir("images"))
+	http.Handle("/images/", http.StripPrefix("/images/", imageServer))
 
 	httpClient := http.Client{
 		Timeout: time.Second * 8, // define timeout
@@ -120,6 +121,7 @@ func main() {
 		//imageServer := http.FileServer(http.Dir("images"))
 		//http.Handle("/images/", http.StripPrefix("/images/", imageServer))
 		tmpl = template.Must(template.ParseFiles("static/html/lore.html"))
+		fmt.Println(response.Data[id])
 		tmpl.Execute(w, response.Data[id])
 	})
 
